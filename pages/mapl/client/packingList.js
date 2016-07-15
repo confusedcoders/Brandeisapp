4945f86f27866291e6c1fb853076fe3d4698884a
mitts425 <waverunnercurl@me.com>
2016-07-15 16:55:39 -0400

    changes with packing list

--- pages/mapl/client/packingList.js
+++ pages/mapl/client/packingList.js
@@ -6,19 +6,37 @@ Template.packingList.helpers(
    )
   
  Template.packingList.events({
- 	"click .js-submit-packinglist": 
- 		function(event) {
+ 	"click .js-submit-packinglist": function(event) {
  			console.log("yes");
  			const list = $(".js-list").val();
  			console.log(list); 
-  			const item = {item:list, createdBy:Meteor.userId()};
+  			const item = {item:list, createdBy:Meteor.userId(), current:false};
   			console.dir(item);
   			userinfo.insert(item);
+
  	},
- 	"click #resetlist":function(){ console.log("click")
+ 		"click #resetlist":function(){ console.log("click")
  		Meteor.call("remove",Meteor.userId())
-  	}
-  	//"click .js-delete":function(event){
-  	//PackingList.remove(this_id);
- 	//}
+ 	},
+ });
+ Template.question.helpers({
+ 	checked: function(){
+ 		if (this.current) return "checked"; else return "";},
+ 		ischecked:function(){return Meteor.user().profile.ischecked;}
+
+
  })
+Template.question.events({
+	"click .js-packed": function(event){
+		var theCurrentValue = event.target.checked;
+		console.log ("theCurrentValue=" + theCurrentValue);
+		userinfo.update(this.user._id, {$set:{current:theCurrentValue}});
+		Meteor.users.update({_id: userinfo._id}, {$set: {"profile.ischecked": ischecked}});
+		  
+ 
+	
+	},
+	"click #deleteitem":function(event){console.log(this)
+  	userinfo.remove(this.user._id);
+ 	}
+});
