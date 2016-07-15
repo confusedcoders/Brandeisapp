a04625e0036f5b290bf174b6e9464f51615b8a6d
hmachmali <test@Hilas-MacBook-Pro.local>
2016-07-15 16:07:31 -0400

    Remembering the Whereugoin and making the button lead to packing list

--- pages/whereugoin/client/whereugoing.js
+++ pages/whereugoin/client/whereugoing.js
@@ -11,23 +11,23 @@ Template.whereugoin.events({
 	"click .js-submit-tripinfo": 
 		function(event) {
 			console.log("yes");
+			const tripname = $(".js-tripname").val();
 			const destination = $(".js-destination").val();
 			const returndate = $(".js-returndate").val();
 			const leaving = $(".js-leaving").val();
 			const people = $(".js-people").val();
 			const triptype = $(".js-triptype").val();
-			const tripname = $(".js-tripname").val();
 				
 			console.log(tripname);
 			console.log(destination); 
 			console.log(leaving); 
 			console.log(people); 
 			console.log(triptype); 
-			const item = {tripname:name, destination:destination, leaving:leaving, returndate:returndate, people:people, triptype:triptype}
+			const item = {tripname:tripname, destination:destination, leaving:leaving, returndate:returndate, people:people, triptype:triptype}
 			console.dir(item);
 			TripInfo.insert(item);
 	},
-"click .js-delete":function(event){
-		UserInfo.remove(this_id); 
-	}
+//"click .js-delete":function(event){
+//		UserInfo.remove(this_id); 
+//	}
 })
