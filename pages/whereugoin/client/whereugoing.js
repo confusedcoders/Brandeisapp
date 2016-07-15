4d266794d2291d7d9832b80e98fd82fc0da6764b
hmachmali <test@Hilas-MacBook-Pro.local>
2016-07-15 10:29:12 -0400

    Where are you going page

--- pages/whereugoin/client/whereugoing.js
+++ pages/whereugoin/client/whereugoing.js
@@ -12,17 +12,18 @@ Template.whereugoing.events({
 		function(event) {
 			console.log("yes");
 			const destination = $(".js-destination").val();
-			const leaving = $(".js-leaving").val();
+			const returndate = $(".js-returndate").val();
+			const leaving = $(".js-returndate").val();
 			const people = $(".js-people").val();
 			const triptype = $(".js-triptype").val();
-			const triptype = $(".js-tripname").val();
+			const tripname = $(".js-tripname").val();
 				
 			console.log(tripname);
 			console.log(destination); 
 			console.log(leaving); 
 			console.log(people); 
 			console.log(triptype); 
-			const item = {tripname:name, destination:destination, leaving:leaving, people:people, triptype:triptype}
+			const item = {tripname:name, destination:destination, leaving:leaving, returndate:returndate, people:people, triptype:triptype}
 			console.dir(item);
 			TripInfo.insert(item);
 	},
