7a2a09ba81f0ef8e54dc40aac446eeca7e99a59e
hmachmali <test@Hilas-MacBook-Pro.local>
2016-07-19 10:50:19 -0400

    Omari's stuff???

--- pages/packtips/client/packtips.js
+++ pages/packtips/client/packtips.js
@@ -1,42 +1,33 @@
-Template.packingList.helpers(
-   {
-     users: function(){
-   	return UserInfo.find({createdBy:Meteor.userId()});
-   }}
-   )
-  
- Template.packingList.events({
- 	"click .js-submit-packinglist": function(event) {
- 			console.log("yes");
- 			const list = $(".js-list").val();
- 			console.log(list); 
-  			const item = {item:list, createdBy:Meteor.userId(), current:false};
-  			console.dir(item);
-  			UserInfo.insert(item);
+Template.chat.helpers({
+  chatlines: function(){
+    // return the last five chats
+    // sorted by when they were created (most recent, first)
+    return Chats.find({},
+                      {limit:500,
+                        sort: {createdAt: -1}})},
 
- 	},
- 		"click #resetlist":function(){ console.log("click")
- 		Meteor.call("remove",Meteor.userId())
- 	},
- });
- Template.question.helpers({
- 	checked: function(){
- 		if (this.current) return "checked"; else return "";},
- 		ischecked:function(){return Meteor.user().profile.ischecked;}
+})
 
 
- })
-Template.question.events({
-	"click .js-packed": function(event){
-		var theCurrentValue = event.target.checked;
-		console.log ("theCurrentValue=" + theCurrentValue);
-		UserInfo.update(this.user._id, {$set:{current:theCurrentValue}});
-		Meteor.users.update({_id: UserInfo._id}, {$set: {"profile.ischecked": ischecked}});
-		  
- 
-	
-	},
-	"click #deleteitem":function(event){console.log(this)
-  	UserInfo.remove(this.user._id);
- 	}
-});
+Template.chat.events({
+  "click .js-chatsubmit": function(event){
+    event.preventDefault();
+    console.log("the button was clicked")
+    const theText = $(".js-chatinput").val();  // read the user's chat text ...
+    const chatline = {text:theText, createdAt:new Date(), createdBy:Meteor.userId()};
+    console.dir(chatline);
+    Chats.insert(chatline);
+  },
+
+  "click .js-clear": function(event) {
+    Meteor.call("clearChats");
+  }
+
+  "click .js-delete": function(event){
+    console.log("clicked this!");
+    console.dir(this);
+    Chats.remove(this.chat._id);
+  }
+
+})
+
