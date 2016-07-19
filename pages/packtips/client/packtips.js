a4c5162efcc7b15148dc2cf2926fb3384d4e2ab6
mitts425 <waverunnercurl@me.com>
2016-07-19 17:20:56 -0400

    pack tips

--- pages/packtips/client/packtips.js
+++ pages/packtips/client/packtips.js
@@ -6,28 +6,54 @@ Template.chat.helpers({
                       {limit:500,
                         sort: {createdAt: -1}})},
 
-})
+ 	
+ 		"click #resetlist":function(){ console.log("click")
+ 		Meteor.call("remove",Meteor.userId())
+ 	},
+ });
 
-
-Template.chat.events({
+Template.packtips.events({
   "click .js-chatsubmit": function(event){
     event.preventDefault();
     console.log("the button was clicked")
     const theText = $(".js-chatinput").val();  // read the user's chat text ...
     const chatline = {text:theText, createdAt:new Date(), createdBy:Meteor.userId()};
-    console.dir(chatline);
     Chats.insert(chatline);
   },
 
-  "click .js-clear": function(event) {
-    Meteor.call("clearChats");
-  }
+})
 
-  "click .js-delete": function(event){
-    console.log("clicked this!");
-    console.dir(this);
-    Chats.remove(this.chat._id);
-  }
+ Template.question.helpers({
+ 	checked: function(){
+ 		if (this.current) return "checked"; else return "";},
+ 		ischecked:function(){return Meteor.user().profile.ischecked;}
+
+
+ })
+
+Template.packtips.helpers({
+  chatlines: function(){
+    // return the last five chats
+    // sorted by when they were created (most recent, first)
+    return Chats.find({},
+                      {limit:5,
+                        sort: {createdAt: -1}})},
 
 })
+
+
+Template.question.events({
+	"click .js-packed": function(event){
+		var theCurrentValue = event.target.checked;
+		console.log ("theCurrentValue=" + theCurrentValue);
+		UserInfo.update(this.user._id, {$set:{current:theCurrentValue}});
+		Meteor.users.update({_id: UserInfo._id}, {$set: {"profile.ischecked": ischecked}});
+		  
+ 
+	
+	},
+	"click #deleteitem":function(event){console.log(this)
+  	UserInfo.remove(this.user._id);
+ 	}
+});
 
